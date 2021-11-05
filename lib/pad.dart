c93adfca847b702ad4dd190ffa3f3448e5846e90
jgdml <jgdml2801@gmail.com>
2021-11-05 11:25:41 -0300

    Tela de resultados funcionando

--- lib/pad.dart
+++ lib/pad.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
@@ -168,9 +171,12 @@ class _HomeState extends State<Home> {
             child: RoundIconButton(
               label: "CALCULAR",
               onPressed: () {
+                var calc = Calculator(height: height, weight: weight);
+                calc.calculate();
+
                 Navigator.of(context).push(
                   MaterialPageRoute(
-                    builder: (context) => const Results(),
+                    builder: (context) => Results(result: calc.getResult(), value: calc.getValue(), description: calc.getDescription()),
                   ),
                 );
               },
@@ -184,8 +190,52 @@ class _HomeState extends State<Home> {
   }
 }
 
+class Calculator {
+    Calculator({required this.height, required this.weight});
+
+    final int weight;
+    final int height;
+
+    late double _bmi;
+
+    void calculate() {
+        double res = weight / (pow(height / 100, 2));
+        _bmi = res;
+    }
+
+    String getValue(){
+        return _bmi.toStringAsFixed(1);
+    }
+
+    String getResult() {
+        if (_bmi >= 25) {
+            return "Acima do peso";
+        } else if (_bmi > 18.5) {
+            return "Normal";
+        } else {
+            return "Abaxio do peso";
+        }
+    }
+
+    String getDescription() {
+        if (_bmi >= 25) {
+            return 'Você tem um peso acima do normal. Tente se exercitar mais.';
+        } else if (_bmi >= 18.5) {
+            return 'Você tem um peso normal. Parabéns.';
+        } else {
+            return 'Você tem um peso abaixo do normal. Você pode comer um pouco mais.';
+        }
+    }
+}
+
+
+
 class Results extends StatelessWidget {
-  const Results({Key? key}) : super(key: key);
+  Results({required this.result, required this.value, required this.description});
+
+  final String result;
+  final String value;
+  final String description;
 
   @override
   Widget build(BuildContext context) {
@@ -206,15 +256,15 @@ class Results extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    "Normal",
+                    result,
                     style: kResultStyle,
                   ),
-                  const Text(
-                    "22.4",
+                  Text(
+                    value,
                     style: kValueStyle,
                   ),
-                  const Text(
-                    "You have a normal body weight. Good job!",
+                  Text(
+                    description,
                     style: kDescriptionStyle,
                     textAlign: TextAlign.center,
                   ),
